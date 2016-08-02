0442f1eeae7bb453ab35f6c916e62ff402c38e0f
Gerasimenko Mike <github@gerasimenko.me>
2016-08-02 16:26:03 +0200

    Fix for crash in user image transcoder

--- Source/Synchronization/Transcoders/ZMUserImageTranscoder.m
+++ Source/Synchronization/Transcoders/ZMUserImageTranscoder.m
@@ -76,40 +76,43 @@ static NSString * const RequestUserProfileSmallAssetNotificationName = @"ZMReque
     self = [super initWithManagedObjectContext:moc];
     if (self != nil) {
         
-        [self recoverFromInconsistentUserImageStatus];
-                
-        _imageProcessingQueue = imageProcessingQueue;
-        
-        // Small profiles
-        NSPredicate *filterForSmallImage = [NSCompoundPredicate andPredicateWithSubpredicates:@[
-                                            [ZMUser predicateForSmallImageNeedingToBeUpdatedFromBackend],
-                                            [ZMUser predicateForSmallImageDownloadFilter]
-                                            ]];
-        self.smallProfileDownstreamSync = [[ZMDownstreamObjectSyncWithWhitelist alloc] initWithTranscoder:self
-                                                                                               entityName:ZMUser.entityName
-                                                                            predicateForObjectsToDownload:filterForSmallImage
-                                                                                     managedObjectContext:self.managedObjectContext];
-        [self.smallProfileDownstreamSync whiteListObject:[ZMUser selfUserInContext:moc]];
-        
-        // Medium profile
-        NSPredicate *filterForMediumImage = [NSCompoundPredicate andPredicateWithSubpredicates:@[
-                                                                                                 [ZMUser predicateForMediumImageNeedingToBeUpdatedFromBackend],
-                                                                                                 [ZMUser predicateForMediumImageDownloadFilter]
-                                                                                                 ]];
-        self.mediumDownstreamSync = [[ZMDownstreamObjectSyncWithWhitelist alloc] initWithTranscoder:self
-                                                                                         entityName:ZMUser.entityName
-                                                                      predicateForObjectsToDownload:filterForMediumImage
-                                                                               managedObjectContext:self.managedObjectContext];
-        [self.mediumDownstreamSync whiteListObject:[ZMUser selfUserInContext:moc]];
-        
-        // Self user upstream
-        self.upstreamSync = [[ZMUpstreamAssetSync alloc] initWithTranscoder:self entityName:ZMUser.entityName keysToSync:@[ImageSmallProfileDataKey, ImageMediumDataKey] managedObjectContext:self.managedObjectContext];
-        
-        _assetPreprocessingTracker = [self createAssetPreprocessingTracker];
-        
-        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(requestAssetForNotification:) name:RequestUserProfileAssetNotificationName object:nil];
-        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(requestSmallAssetForNotification:) name:RequestUserProfileSmallAssetNotificationName object:nil];
-        
+        [self.managedObjectContext performBlockAndWait:^{
+            [self recoverFromInconsistentUserImageStatus];
+            
+            self->_imageProcessingQueue = imageProcessingQueue;
+            
+            // Small profiles
+            NSPredicate *filterForSmallImage = [NSCompoundPredicate andPredicateWithSubpredicates:@[
+                                                                                                    [ZMUser predicateForSmallImageNeedingToBeUpdatedFromBackend],
+                                                                                                    [ZMUser predicateForSmallImageDownloadFilter]
+                                                                                                    ]];
+            self.smallProfileDownstreamSync = [[ZMDownstreamObjectSyncWithWhitelist alloc] initWithTranscoder:self
+                                                                                                   entityName:ZMUser.entityName
+                                                                                predicateForObjectsToDownload:filterForSmallImage
+                                                                                         managedObjectContext:self.managedObjectContext];
+            [self.smallProfileDownstreamSync whiteListObject:[ZMUser selfUserInContext:moc]];
+            
+            // Medium profile
+            NSPredicate *filterForMediumImage = [NSCompoundPredicate andPredicateWithSubpredicates:@[
+                                                                                                     [ZMUser predicateForMediumImageNeedingToBeUpdatedFromBackend],
+                                                                                                     [ZMUser predicateForMediumImageDownloadFilter]
+                                                                                                     ]];
+            self.mediumDownstreamSync = [[ZMDownstreamObjectSyncWithWhitelist alloc] initWithTranscoder:self
+                                                                                             entityName:ZMUser.entityName
+                                                                          predicateForObjectsToDownload:filterForMediumImage
+                                                                                   managedObjectContext:self.managedObjectContext];
+            [self.mediumDownstreamSync whiteListObject:[ZMUser selfUserInContext:moc]];
+            
+            // Self user upstream
+            self.upstreamSync = [[ZMUpstreamAssetSync alloc] initWithTranscoder:self entityName:ZMUser.entityName keysToSync:@[ImageSmallProfileDataKey, ImageMediumDataKey] managedObjectContext:self.managedObjectContext];
+            
+            self->_assetPreprocessingTracker = [self createAssetPreprocessingTracker];
+            
+            [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(requestAssetForNotification:) name:RequestUserProfileAssetNotificationName object:nil];
+            [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(requestSmallAssetForNotification:) name:RequestUserProfileSmallAssetNotificationName object:nil];
+            
+        }];
+       
         
     }
     return self;
@@ -117,13 +120,13 @@ static NSString * const RequestUserProfileSmallAssetNotificationName = @"ZMReque
 
 - (void)recoverFromInconsistentUserImageStatus;
 {
-    [self.managedObjectContext performGroupedBlock:^{
-        NSFetchRequest *request = [ZMUser sortedFetchRequestWithPredicateFormat:@"(modifiedDataFields != 0) AND (%K == NULL OR %K == NULL)", ImageMediumDataKey, ImageSmallProfileDataKey];
-        NSArray *corruptUsers = [self.managedObjectContext executeFetchRequestOrAssert:request];
-        for (ZMUser *user in corruptUsers) {
-            [user resetLocallyModifiedKeys:[NSSet setWithArray:@[ImageMediumDataKey,ImageSmallProfileDataKey]]];
-        }
-    }];
+    NSSet *imageMediumKeys = [NSSet setWithArray:@[ImageMediumDataKey,ImageSmallProfileDataKey]];
+    
+    ZMUser *selfUser = [ZMUser selfUserInContext:self.managedObjectContext];
+    
+    if ([selfUser hasLocalModificationsForKeys:imageMediumKeys] && (selfUser.imageMediumData == nil || selfUser.imageSmallProfileData == nil)) {
+        [selfUser resetLocallyModifiedKeys:imageMediumKeys];
+    }
 }
 
 - (ZMImagePreprocessingTracker *)createAssetPreprocessingTracker
