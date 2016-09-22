bcd51733abb6df90ce1e719e6dbb67b847b01833
Silvan Dähn <silvandaehn@me.com>
2016-09-22 17:03:20 +0200

    Increase timeout and print logs in case of failure

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 set -o pipefail
-xcodebuild clean build test -scheme zmessaging-ios -destination 'platform=iOS Simulator,name=iPhone 7,OS=10.0' | xcpretty
+xcodebuild clean build test -scheme zmessaging-ios -destination 'platform=iOS Simulator,name=iPhone 7,OS=10.0' | tee >build.log >(xcpretty)
