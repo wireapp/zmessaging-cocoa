287f76a8ef266008faef3b3c172e97dc8c67f16c
Jacob Persson <jacob.persson@wire.com>
2016-11-17 16:07:05 +0100

    Changes due to re-factoring for share extension

--- Tests/Source/UserSession/Search/ZMUserIDsForSearchDirectoryTableTests.m
+++ Tests/Source/UserSession/Search/ZMUserIDsForSearchDirectoryTableTests.m
@@ -115,7 +115,7 @@
         
         id mockUserSession = [OCMockObject mockForClass:ZMUserSession.class];
         [[[mockUserSession stub] andReturn:self.syncMOC] syncManagedObjectContext];
-        [[mockUserSession expect] databaseDirectoryURL];
+        [[mockUserSession expect] storeURL];
         [[mockUserSession expect] managedObjectContext];
 
         ZMSearchDirectory *directory = [[ZMSearchDirectory alloc] initWithUserSession:mockUserSession];
