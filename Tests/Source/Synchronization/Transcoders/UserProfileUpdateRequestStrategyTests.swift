5741184d05b16170dabfc8600f549e511e40ac14
Vytis Šibonis <vytis@proto.lt>
2017-01-24 11:52:29 +0100

    Added method to change user email

--- Tests/Source/Synchronization/Transcoders/UserProfileUpdateRequestStrategyTests.swift
+++ Tests/Source/Synchronization/Transcoders/UserProfileUpdateRequestStrategyTests.swift
@@ -358,7 +358,24 @@ extension UserProfileUpdateRequestStrategyTests {
         XCTAssertEqual(self.userProfileUpdateStatus.recordedDidFailPasswordUpdate , 1)
     }
     
-    func testThatItCallsDidUpdateEmailSuccessfully() {
+    func testThatItCallsDidUpdateEmailSuccessfullyWhenChangingEmail() {
+        
+        // GIVEN
+        let selfUser = ZMUser.selfUser(in: self.uiMOC)
+        selfUser.emailAddress = "foo@email.com"
+        try! self.userProfileUpdateStatus.requestEmailChange(email: "mario@example.com")
+        XCTAssertTrue(self.waitForAllGroupsToBeEmpty(withTimeout: 0.2))
+        
+        // WHEN
+        let request = self.sut.nextRequest()
+        request?.complete(with: self.successResponse())
+        
+        // THEN
+        XCTAssertTrue(self.waitForAllGroupsToBeEmpty(withTimeout: 0.5))
+        XCTAssertEqual(self.userProfileUpdateStatus.recordedDidUpdateEmailSuccessfully , 1)
+    }
+    
+    func testThatItCallsDidUpdateEmailSuccessfullyWhenSettingEmailAndPassword() {
         
         // GIVEN
         let credentials = ZMEmailCredentials(email: "mario@example.com", password: "princess")
