010a28a43cb9d1d5c64ccc03744f2e1d74e1b139
Vytis Šibonis <vytis@proto.lt>
2017-01-26 16:33:41 +0100

    No need to re-sync the user after email deletion

--- Source/Synchronization/Strategies/UserProfileUpdateRequestStrategy.swift
+++ Source/Synchronization/Strategies/UserProfileUpdateRequestStrategy.swift
@@ -218,7 +218,6 @@ extension UserProfileRequestStrategy : ZMSingleRequestTranscoder {
             
         case self.emailDeleteSync:
             if response.result == .success {
-                ZMUser.selfUser(in: managedObjectContext).needsToBeUpdatedFromBackend = true
                 self.userProfileUpdateStatus.didRemoveEmailSuccessfully()
             } else {
                 let error : Error = NSError.lastUserIdentityCantBeRemoved(with: response) ??
