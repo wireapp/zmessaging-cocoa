90fa2868a1bc46c05ad7b7983dc97f4cb24201ad
Marco Conti <marcoconti83@gmail.com>
2017-03-05 09:39:14 +0100

    Fix tests

--- Tests/Source/Synchronization/Sync States/ZMBackgroundStateTests.m
+++ Tests/Source/Synchronization/Sync States/ZMBackgroundStateTests.m
@@ -122,7 +122,11 @@
 {
     // given
     for (id transcoder in self.syncObjectsUsedByState) {
-        [[[transcoder stub] andReturn:@[]] requestGenerators];
+        if([transcoder conformsToProtocol:@protocol(ZMRequestGeneratorSource)]) {
+            [[[transcoder stub] andReturn:@[]] requestGenerators];
+        } else {
+            [[transcoder stub] nextRequest];
+        }
     }
     
     // expect
@@ -139,9 +143,14 @@
 {
     // given
     id<ZMRequestGenerator> generator = [self generatorReturningNiceMockRequest];
+    ZMTransportRequest *dummyRequest = [ZMTransportRequest requestGetFromPath:@"foobar"];
     
     for (id transcoder in self.syncObjectsUsedByState) {
-        [[[transcoder stub] andReturn:@[generator]] requestGenerators];
+        if([transcoder conformsToProtocol:@protocol(ZMRequestGeneratorSource)]) {
+            [[[transcoder stub] andReturn:@[generator]] requestGenerators];
+        } else {
+            [[[transcoder stub] andReturn:dummyRequest] nextRequest];
+        }
     }
     
     // expect
@@ -158,7 +167,11 @@
 {
     // given
     for (id transcoder in self.syncObjectsUsedByState) {
-        [[[transcoder stub] andReturn:nil] requestGenerators];
+        if([transcoder conformsToProtocol:@protocol(ZMRequestGeneratorSource)]) {
+            [[[transcoder stub] andReturn:@[]] requestGenerators];
+        } else {
+            [[transcoder stub] nextRequest];
+        }
     }
     
     // expect
@@ -178,7 +191,11 @@
 {
     // given
     for (id transcoder in self.syncObjectsUsedByState) {
-        [[[transcoder stub] andReturn:nil] requestGenerators];
+        if([transcoder conformsToProtocol:@protocol(ZMRequestGeneratorSource)]) {
+            [[[transcoder stub] andReturn:@[]] requestGenerators];
+        } else {
+            [[transcoder stub] nextRequest];
+        }
     }
     
     // expect
