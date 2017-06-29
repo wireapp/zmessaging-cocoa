762cd6dd2a064a7f4c6ceb64eae6f9a5e5bad579
Jacob Persson <jacob.persson@wire.com>
2017-06-29 19:54:38 +0200

    Bump to mock transport without calling V2

--- Tests/Source/Integration/PushChannelTests.m
+++ Tests/Source/Integration/PushChannelTests.m
@@ -72,7 +72,7 @@
     
     [self.mockTransportSession performRemoteChanges:^(MockTransportSession<MockTransportSessionObjectCreation> *session) {
         NOT_USED(session);
-        [self.groupConversation addUserToCall:self.user1];
+        [session sendIsTypingEventForConversation:self.groupConversation user:self.user1 started:YES];
     }];
     WaitForAllGroupsToBeEmpty(0.5);
     
@@ -93,7 +93,7 @@
         XCTAssertEqualObjects(messageEvent.payload[@"type"], @"conversation.otr-message-add");
         
         // will create a transient notification
-        [self.groupConversation addUserToCall:self.user2]; // TODO jacob - don't use deprecicated call events here
+        [session sendIsTypingEventForConversation:self.groupConversation user:self.user1 started:NO];
         
     }];
     WaitForAllGroupsToBeEmpty(0.5);
@@ -109,8 +109,6 @@
     // then
     NSString *expectedLastRequest = [NSString stringWithFormat:@"/notifications?size=%lu&since=%@&client=%@", ZMMissingUpdateEventsTranscoderListPageSize, messageAddLastNotificationID.transportString, self.userSession.selfUserClient.remoteIdentifier];
     XCTAssertEqualObjects([(ZMTransportRequest *)self.mockTransportSession.receivedRequests.lastObject path], expectedLastRequest);
-//    ZMConversation *syncConv = (id)[self.userSession.syncManagedObjectContext objectWithID:[self conversationForMockConversation:self.groupConversation].objectID];
-//    [syncConv.voiceChannelRouter.v2 tearDown];
 }
 
 
