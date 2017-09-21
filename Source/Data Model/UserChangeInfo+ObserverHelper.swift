4f2bc617f7abdd3dfd04432c6ef31df47bcdf2f9
Gerasimenko Mike <mike@gerasimenko.me>
2017-09-21 12:14:53 +0200

    User session - based observation

--- Source/Data Model/UserChangeInfo+ObserverHelper.swift
+++ Source/Data Model/UserChangeInfo+ObserverHelper.swift
@@ -1,9 +1,19 @@
 //
-//  UserChangeInfo.swift
-//  WireSyncEngine
+// Wire
+// Copyright (C) 2017 Wire Swiss GmbH
 //
-//  Created by Jacob on 19.09.17.
-//  Copyright © 2017 Zeta Project Gmbh. All rights reserved.
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
 //
 
 import Foundation
@@ -26,5 +36,4 @@ public extension UserChangeInfo {
     static func add(searchUserObserver observer: ZMUserObserver, for user: ZMSearchUser?, userSession: ZMUserSession) -> NSObjectProtocol {
         return UserChangeInfo.add(searchUserObserver: observer, for: user, managedObjectContext: userSession.searchManagedObjectContext)
     }
-    
 }
