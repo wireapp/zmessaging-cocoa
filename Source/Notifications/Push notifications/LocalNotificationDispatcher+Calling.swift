b4e15039c1d25dd9b36aac16822831bb1a5aa8a8
John Nguyen <johnxuannguyen@gmail.com>
2017-10-10 17:22:46 +0200

    update notification dispatcher callState methods

--- Source/Notifications/Push notifications/LocalNotificationDispatcher+Calling.swift
+++ Source/Notifications/Push notifications/LocalNotificationDispatcher+Calling.swift
@@ -21,43 +21,14 @@ import Foundation
 public extension LocalNotificationDispatcher {
     
     public func process(callState: CallState, in conversation: ZMConversation, sender: ZMUser) {
-        
         let note = ZMLocalNote(callState: callState, conversation: conversation, sender: sender)
+        callingNotifications.cancelNotifications(conversation)
         note.apply(scheduleLocalNotification)
-        
-        // TODO: cancelling other call notes for this conversation?
-//        callingNotifications.cancelNotifications(conversation)
     }
     
     public func processMissedCall(in conversation: ZMConversation, sender: ZMUser) {
-        
         let note = ZMLocalNote(callState: .terminating(reason: .canceled), conversation: conversation, sender: sender)
+        callingNotifications.cancelNotifications(conversation)
         note.apply(scheduleLocalNotification)
-        
-        // cancel all other call notifications for this conversation?
-//        callingNotifications.cancelNotifications(conversation)
-    }
-    
-    private func notification(for conversation: ZMConversation, sender: ZMUser) -> ZMLocalNotificationForCallState {
-        if let callStateNote = callingNotifications.notifications.first(where: { $0.isCallStateNote(for: conversation, by: sender) }) as? ZMLocalNotificationForCallState {
-            return callStateNote
-        }
-        
-        return ZMLocalNotificationForCallState(conversation: conversation, sender: sender)
-    }
-    
-}
-
-extension ZMLocalNotification {
-    
-    /// Returns whether this notification is a call state notification matching conversation and sender
-    fileprivate func isCallStateNote(for conversation: ZMConversation, by sender: ZMUser) -> Bool {
-        guard let callStateNotification = self as? ZMLocalNotificationForCallState,
-            callStateNotification.conversation == conversation,
-            callStateNotification.sender == sender
-        else {
-                return false
-        }
-        return true
     }
 }
