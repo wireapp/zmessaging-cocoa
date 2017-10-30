4646e1ac22082645c55502c7eb8effef5508e59f
Bill Chan <billypchan@gmail.com>
2017-10-30 19:08:55 +0100

    fix: set to foreground when testThatItForwardsNotificationFromMessagesIfActive()

--- Tests/Source/Notifications/PushNotifications/LocalNotificationDispatcherTests.swift
+++ Tests/Source/Notifications/PushNotifications/LocalNotificationDispatcherTests.swift
@@ -104,7 +104,7 @@ extension LocalNotificationDispatcherTests {
         let text = UUID.create().transportString()
         let message = self.conversation1.appendMessage(withText: text) as! ZMClientMessage
         message.sender = self.user1
-        self.mockUserSession.operationStatus?.isInBackground = true
+        self.mockUserSession.operationStatus?.isInBackground = false
         
         // WHEN
         self.sut.process(message)
