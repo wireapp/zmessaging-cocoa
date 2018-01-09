b1f5f8580cac3ef21b468a07a3299421a8bf225d
Jacob Persson <jacob.persson@wire.com>
2018-01-09 11:10:19 +0100

    Don't tear down the unathuthenticated session when client is registered

--- Source/SessionManager/SessionManager.swift
+++ Source/SessionManager/SessionManager.swift
@@ -665,9 +665,7 @@ extension SessionManager: UnauthenticatedSessionDelegate {
 extension SessionManager: PostLoginAuthenticationObserver {
 
     @objc public func clientRegistrationDidSucceed(accountId: UUID) {
-        log.debug("Tearing down unauthenticated session as reaction to successful client registration")
-        unauthenticatedSession?.tearDown()
-        unauthenticatedSession = nil
+        log.debug("Client registration was successful")
     }
     
     public func accountDeleted(accountId: UUID) {
