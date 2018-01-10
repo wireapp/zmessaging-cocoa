a0f3a2a9b6319da4ed6ccfb8c493345a025bbf08
Vytis Šibonis <vytis@wire.com>
2018-01-10 16:56:37 +0100

    Added tracker object

---
+++ Source/Analytics/NotificationsTracker.swift
@@ -0,0 +1,74 @@
+//
+// Wire
+// Copyright (C) 2018 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import Foundation
+
+@objc public class NotificationsTracker: NSObject {
+
+    let eventName = "notifications.processing"
+
+    enum Attributes: String {
+        case startedProcessing
+        case startedFetchingStream
+        case finishedFetchingStream
+        case finishedProcessing
+    }
+
+    weak var analytics: AnalyticsType?
+    @objc public init(analytics: AnalyticsType) {
+        self.analytics = analytics
+    }
+
+    public func registerReceivedPush() {
+        increment(attribute: .startedProcessing)
+    }
+
+    public func registerNotificationProcessingCompleted() {
+        increment(attribute: .finishedProcessing)
+    }
+
+    public func registerFinishStreamFetching() {
+        increment(attribute: .finishedFetchingStream)
+    }
+
+    @objc public func registerStartStreamFetching() {
+        increment(attribute: .startedFetchingStream)
+    }
+
+    private func increment(attribute: Attributes, by amount: Double = 1) {
+        var currentAttributes = analytics?.persistedAttributes(for: eventName) ?? [:]
+        let identifier = "notifications_" + attribute.rawValue
+        var value = (currentAttributes[identifier] as? Double) ?? 0
+        value += amount
+        currentAttributes[identifier] = value as NSObject
+        analytics?.setPersistedAttributes(currentAttributes, for: eventName)
+    }
+
+    public func dispatchEvent() {
+        if let analytics = analytics, let attributes = analytics.persistedAttributes(for: eventName), !attributes.isEmpty {
+            analytics.tagEvent(eventName, attributes: attributes)
+            analytics.setPersistedAttributes(nil, for: eventName)
+        }
+    }
+}
+
+extension NotificationsTracker {
+    override public var debugDescription: String {
+        return "Current values: \(analytics?.persistedAttributes(for: eventName) ?? [:])"
+    }
+}
