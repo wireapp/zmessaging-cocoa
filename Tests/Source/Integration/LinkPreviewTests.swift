8513b79695f35d9c9ca72f97d6c10f5fc66e21f3
Jacob Persson <jacob.persson@gmail.com>
2018-09-26 10:46:34 +0200

    Bump components

---
+++ Tests/Source/Integration/LinkPreviewTests.swift
@@ -0,0 +1,9 @@
+//
+//  LinkPreviewTests.swift
+//  WireSyncEngine-iOS-Tests
+//
+//  Created by Jacob Persson on 26.09.18.
+//  Copyright © 2018 Zeta Project Gmbh. All rights reserved.
+//
+
+import Foundation
