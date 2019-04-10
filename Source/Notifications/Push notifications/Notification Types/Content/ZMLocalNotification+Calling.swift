8a6f2e38c83c5e0fb30bdef49be272a0e8c15615
Jacob Persson <jacob.persson@gmail.com>
2019-04-10 13:24:41 +0200

    Don't show call notifications when away

--- Source/Notifications/Push notifications/Notification Types/Content/ZMLocalNotification+Calling.swift
+++ Source/Notifications/Push notifications/Notification Types/Content/ZMLocalNotification+Calling.swift
@@ -52,6 +52,8 @@ extension ZMLocalNotification {
         }
         
         func shouldCreateNotification() -> Bool {
+            guard ZMUser.selfUser(in: managedObjectContext).availability != .away else { return false }
+            
             switch callState {
             case .terminating(reason: .anweredElsewhere), .terminating(reason: .normal), .terminating(reason: .rejectedElsewhere):
                 return false
