51e68bff3ea1369f96f327498ba1273ff4a4c832
Jacob Persson <jacob.persson@gmail.com>
2019-05-17 11:28:06 +0200

    Update after introduction of DeviceClass and DeviceType enum

--- Tests/Source/Data Model/ServiceUserTests.swift
+++ Tests/Source/Data Model/ServiceUserTests.swift
@@ -39,6 +39,8 @@ final class DummyServiceUser: NSObject, ServiceUser {
     
     var isWirelessUser: Bool = false
     
+    var isUnderLegalHold: Bool = false
+    
     var expiresAfter: TimeInterval = 0
     
     var readReceiptsEnabled: Bool = true
