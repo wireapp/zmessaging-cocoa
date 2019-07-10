ca16b7506a6664e0e6c56ed04026dfd564f7b834
Bill Chan <billypchan@gmail.com>
2019-07-10 12:07:47 +0200

    draft interface and tests

---
+++ Tests/Source/Synchronization/Strategies/TeamImageAssetUpdateStrategyTests.swift
@@ -0,0 +1,61 @@
+//
+// Wire
+// Copyright (C) 2019 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import Foundation
+
+final class TeamImageAssetUpdateStrategyTests : MessagingTest {
+
+    var sut: WireSyncEngine.TeamImageAssetUpdateStrategy!
+
+    override func setUp() {
+        super.setUp()
+        self.mockApplicationStatus = MockApplicationStatus()
+        self.mockApplicationStatus.mockSynchronizationState = .eventProcessing
+        self.updateStatus = MockImageUpdateStatus()
+
+        sut = TeamImageAssetUpdateStrategy(managedObjectContext: syncMOC,
+                                           applicationStatus: mockApplicationStatus,
+                                           imageUploadStatus: updateStatus)
+
+        self.syncMOC.zm_userImageCache = UserImageLocalCache()
+        self.uiMOC.zm_userImageCache = self.syncMOC.zm_userImageCache
+    }
+
+    override func tearDown() {
+        self.mockApplicationStatus = nil
+        self.updateStatus = nil
+        self.sut = nil
+        self.syncMOC.zm_userImageCache = nil
+        super.tearDown()
+    }
+
+    func testThatItCreatesDownstreamRequestSyncs() {
+    }
+
+    func testThatItWhitelistsUserOnPreviewSyncForImageNotification() {
+    }
+
+    func testThatItCreatesRequestForCorrectAssetIdentifierForImage() {
+    }
+
+    func testThatItUpdatesCorrectUserImageDataForImage() {
+    }
+
+    func testThatItDeletesPreviewProfileAssetIdentifierWhenReceivingAPermanentErrorForImage() {
+    }
+}
