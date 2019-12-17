4de8ed3cca7232cdf097f55f80487e689a4b29b2
Marco Conti <marcoconti83@gmail.com>
2019-12-17 09:02:56 +0100

    Fix crash for teamRolesDownloadRequestStrategy mock

--- Source/Synchronization/Strategies/TeamRolesDownloadRequestStrategy.swift
+++ Source/Synchronization/Strategies/TeamRolesDownloadRequestStrategy.swift
@@ -82,7 +82,7 @@ public final class TeamRolesDownloadRequestStrategy: AbstractRequestStrategy, ZM
         return syncStatus.currentSyncPhase == self.expectedSyncPhase
     }
     
-    private func completeSyncPhaseIfNoTeam() {///TODO: crash here, OCMockObject(WireSyncEngine.SyncStatus) has no currentSyncPhase?
+    private func completeSyncPhaseIfNoTeam() {
         if self.syncStatus.currentSyncPhase == self.expectedSyncPhase && !self.downstreamSync.hasOutstandingItems {
             self.syncStatus.finishCurrentSyncPhase(phase: self.expectedSyncPhase)
         }
