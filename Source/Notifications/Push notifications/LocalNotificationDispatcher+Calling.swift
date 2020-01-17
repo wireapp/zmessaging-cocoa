c9b1b93b231f80ac17d93ef50e487c45b06c8def
John Nguyen <polyxo@protonmail.com>
2020-01-17 09:22:38 +0100

    Don't leak ZMUser in LocalNotificationDispatcher+Calling

--- Source/Notifications/Push notifications/LocalNotificationDispatcher+Calling.swift
+++ Source/Notifications/Push notifications/LocalNotificationDispatcher+Calling.swift
@@ -20,25 +20,26 @@ import Foundation
 
 public extension LocalNotificationDispatcher {
     
-    func process(callState: CallState, in conversation: ZMConversation, caller: ZMUser) {
-        // missed call notification are handled separately
-        // but if call was answered elsewhere then proceed
+    func process(callState: CallState, in conversation: ZMConversation, caller: UserType) {
+        // Missed call notification are handled separately but if call was answered elsewhere then proceed.
         switch callState {
-        case .terminating(reason: let reason):
+        case .terminating(let reason):
             switch reason {
             case .anweredElsewhere, .rejectedElsewhere: break
             default: return
             }
         default: break
         }
-        
+
+        guard let caller = caller as? ZMUser else { return }
         let note = ZMLocalNotification(callState: callState, conversation: conversation, caller: caller)
         callingNotifications.cancelNotifications(conversation)
         note.apply(scheduleLocalNotification)
         note.apply(callingNotifications.addObject)
     }
     
-    func processMissedCall(in conversation: ZMConversation, caller: ZMUser) {
+    func processMissedCall(in conversation: ZMConversation, caller: UserType) {
+        guard let caller = caller as? ZMUser else { return }
         let note = ZMLocalNotification(callState: .terminating(reason: .canceled), conversation: conversation, caller: caller)
         callingNotifications.cancelNotifications(conversation)
         note.apply(scheduleLocalNotification)
