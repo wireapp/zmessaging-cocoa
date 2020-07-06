40b9853b17bd7e2eef63838ea990149293763e3d
KaterinaWire <katerina@wire.com>
2020-07-06 12:42:20 +0200

    Clean up

--- Tests/Source/Synchronization/Transcoders/ZMMissingUpdateEventsTranscoderTests.m
+++ Tests/Source/Synchronization/Transcoders/ZMMissingUpdateEventsTranscoderTests.m
@@ -18,7 +18,6 @@
 
 
 @import WireTransport;
-//@import WireSyncEngine;
 @import WireRequestStrategy;
 
 #import <Foundation/Foundation.h>
@@ -112,10 +111,10 @@ static NSString * const LastUpdateEventIDStoreKey = @"LastUpdateEventID";
     [super tearDown];
 }
 
-//@end
-//
-//
-//@implementation ZMMissingUpdateEventsTranscoderTests (MissingNotifications)
+@end
+
+
+@implementation ZMMissingUpdateEventsTranscoderTests (MissingNotifications)
 
 - (NSUUID *)olderNotificationID {
     return [NSUUID uuidWithTransportString:@"a6526b00-000a-11e5-a837-0800200c9a66"];
