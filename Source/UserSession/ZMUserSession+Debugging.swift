3b0ea983480b58fc3ac3e639cfeb19982cd79008
Marco Conti <marcoconti83@gmail.com>
2020-09-11 17:26:20 +0200

    Simplify command handling

--- Source/UserSession/ZMUserSession+Debugging.swift
+++ Source/UserSession/ZMUserSession+Debugging.swift
@@ -11,75 +11,111 @@ import WireCryptobox
 
 extension ZMUserSession {
     
-    fileprivate static let debugCommands = [
-        DebugCommand(
-            "-logCryptoForClient",
-            description: "Disable extended logging for encryption",
-            execution: enableLogEncryption
-        ),
-        DebugCommand(
-            "+logCryptoForClient",
-            description: "Enable extended logging for encryption",
-            execution: disableLogEncryption
-        ),
-        DebugCommand(
-            "showIdentifier",
-            description: "Return the user and client identifier",
-            execution: showIdentifier
-        ),
-        DebugCommand(
-            "help",
-            description: "List all commands",
-            execution: listCommands
-        )
-    ].dictionary { (key: $0.keyword, value: $0) }
-    
     /// Parses and execute a debug command, which is expected to be
     /// tokenized already (e.g. "print", "foobar")
     public func executeDebugCommand(
         _ command: [String],
-        completionHandler: (DebugCommandResult) -> ()
+        onComplete: (DebugCommandResult) -> ()
     ) {
-                
         guard let keyword = command.first else {
-            completionHandler(DebugCommandResult.UnknownCommand)
+            onComplete(.UnknownCommand)
             return
         }
         
         let arguments = Array(command.dropFirst())
         
-        guard let command = ZMUserSession.debugCommands[keyword] else {
-            completionHandler(DebugCommandResult.UnknownCommand)
+        guard let command = debugCommands[keyword] else {
+            onComplete(.UnknownCommand)
             return
         }
         
-        command.closure(arguments, self, completionHandler)
+        command.execute(
+            arguments: arguments,
+            onComplete: onComplete
+        )
     }
     
+    public func restoreCommandsState() {
+        
+    }
+
+    fileprivate var debugStateUserDefaultsKey: String? {
+        get {
+            guard
+                let identifier = (self.selfUser as! ZMUser).remoteIdentifier
+            else { return nil }
+            return "Wire-debugCommandsState-\(identifier)"
+        }
+
+    }
     
+    /// The debug state persisted for this user
+    fileprivate var savedDebugState: [String: [String: Any]] {
+        get {
+            guard let key = debugStateUserDefaultsKey else { return [:] }
+            return UserDefaults.shared()?
+                .dictionary(forKey: key) as? [String: [String: Any]] ?? [:]
+        }
+        set {
+            guard let key = debugStateUserDefaultsKey else { return }
+            UserDefaults.shared()?.set(newValue, forKey: key)
+        }
+    }
 }
 
-/// A debug command with an explaination
-private struct DebugCommand {
+/// A debug command that can be invoked with arguments
+private protocol DebugCommand {
     
-    typealias DebugCommandExecution = (
-        _ arguments: [String],
-        _ userSession: ZMUserSession,
-        _ completionHandler: ((DebugCommandResult) -> ())
-    ) -> ()
+    /// This is the keyword used to invoke the command
+    var keyword: String { get }
+    /// The user session context in which this command is executed
+    var userSession: ZMUserSession { get }
+        
+    /// This will be called to execute the command
+    func execute(
+        arguments: [String],
+        onComplete: ((DebugCommandResult) -> ())
+    )
+    
+    /// This will be called with any previous persisted state
+    /// when the user session is initialized
+    func restoreFromState(state: [String: Any])
+}
+
+extension DebugCommand {
+    
+    /// Save any "state" that needs to be persisted. The state should
+    /// only contain types can serialized in user defaults.
+    func saveState(state: [String: Any]) {
+        self.userSession.savedDebugState[self.keyword] = state
+    }
+    
+    var savedState: [String: Any]? {
+        get { return self.userSession.savedDebugState[self.keyword] }
+    }
+}
+
+/// This is a mixin (implementation of a protocol that can be
+/// inherited to avoid having to rewrite all protocol methods and vars)
+private class DebugCommandMixin: DebugCommand {
     
     let keyword: String
-    let description: String
-    let closure: DebugCommandExecution
+    let userSession: ZMUserSession
     
     init(
-        _ keyword: String,
-        description: String,
-        execution: @escaping DebugCommandExecution
+        keyword: String,
+        userSession: ZMUserSession
     ) {
         self.keyword = keyword
-        self.description = description
-        self.closure = execution
+        self.userSession = userSession
+    }
+    
+    func execute(arguments: [String], onComplete: ((DebugCommandResult) -> ())) {
+        onComplete(.Failure(error: "Not implemented"))
+    }
+    
+    func restoreFromState(state: [String : Any]) {
+        return
     }
 }
 
@@ -95,74 +131,128 @@ public enum DebugCommandResult {
     case UnknownCommand
 }
 
+
 // MARK: - Commands
 
-private func enableLogEncryption(
-arguments: [String],
-userSession: ZMUserSession,
-completionHandler: (DebugCommandResult) -> ()
-) {
-    setLogEncryption(
-        true,
-        arguments: arguments,
-        userSession: userSession,
-        completionHandler: completionHandler)
+extension ZMUserSession {
+
+    fileprivate var debugCommands: [String: DebugCommand] {
+        get {
+            return [
+                DebugCommandLogEncryption(userSession: self),
+            ].dictionary { (key: $0.keyword, value: $0) }
+        }
+    }
 }
 
-private func disableLogEncryption(
-arguments: [String],
-userSession: ZMUserSession,
-completionHandler: (DebugCommandResult) -> ()
-) {
-    setLogEncryption(
-        true,
-        arguments: arguments,
-        userSession: userSession,
-        completionHandler: completionHandler)
+// MARK: - Command execution
+
+extension EncryptionSessionIdentifier {
+    
+    fileprivate init?(string: String) {
+        let split = string.split(separator: "_")
+        guard split.count == 2 else { return nil }
+        let user = String(split[0])
+        let client = String(split[1])
+        self.init(userId: user, clientId: client)
+    }
 }
 
-/// Enable or disables encryption logs for a given user and session
-private func setLogEncryption(
-    _ enabled: Bool,
-    arguments: [String],
-    userSession: ZMUserSession,
-    completionHandler: (DebugCommandResult) -> ()
-    )
-{
-    guard let context = userSession.selfUserClient?.keysStore.encryptionContext
+private class DebugCommandLogEncryption: DebugCommandMixin {
+    
+    var currentlyEnabledLogs: Set<EncryptionSessionIdentifier> = Set()
+    
+    private var usage: String {
+        get { "\(self.keyword) <add|remove|list> <sessionId|all>"}
+    }
+    
+    init(userSession: ZMUserSession) {
+        super.init(
+            keyword: "logEncryption",
+            userSession: userSession
+        )
+    }
+    
+    override func execute(
+        arguments: [String],
+        onComplete: ((DebugCommandResult) -> ()))
+    {
+        defer {
+            self.saveEnabledLogs()
+        }
+        
+        if (arguments.first == "list") {
+            return onComplete(.Success(info:
+                "Enabled:\n" +
+                self.currentlyEnabledLogs
+                    .map { $0.rawValue }
+                    .joined(separator: "\n")
+                ))
+        }
+        
+        guard arguments.count == 2,
+        arguments[0] == "add" || arguments[0] == "remove"
         else {
-            completionHandler(.Failure(error: "No session"))
-            return
+            return onComplete(.Failure(error: "usage: \(self.usage)"))
+        }
+        
+        let isAdding = arguments[0] == "add"
+        let subject = arguments[1]
+        
+        guard let context = userSession.selfUserClient?.keysStore.encryptionContext
+            else {
+                return onComplete(.Failure(error: "No encryption context?"))
+        }
+        
+        if !isAdding && subject == "all" {
+            context.disableExtendedLoggingOnAllSessions()
+            self.currentlyEnabledLogs = Set()
+            return onComplete(.Success(info: "all removed"))
+        }
+        
+        guard let identifier = EncryptionSessionIdentifier(string: subject) else {
+            return onComplete(.Failure(error: "Invalid id \(subject)"))
+        }
+        
+        if isAdding {
+            self.currentlyEnabledLogs.insert(identifier)
+        } else {
+            self.currentlyEnabledLogs.remove(identifier)
+        }
+        context.setExtendedLogging(identifier: identifier, enabled: isAdding)
+        return onComplete(.Success(info: nil))
     }
     
-    // if there is no argument to the disabling, then disable all
-    if (!enabled && arguments.isEmpty) {
-        context.disableExtendedLoggingOnAllSessions()
-        completionHandler(.Success(info: "Logging disabled for all sessions"))
-        return
+    private let logsKey = "enabledLogs"
+    
+    private func saveEnabledLogs() {
+        let idsToSave = self.currentlyEnabledLogs.map {
+            $0.rawValue
+        }
+        self.saveState(state: [logsKey: idsToSave])
     }
     
-    guard arguments.count == 2 else {
-        completionHandler(.Failure(error: "Expected arguments: <userId> <clientId>"))
-        return
+    override func restoreFromState(state: [String : Any]) {
+        guard let logs = state[logsKey] as? [String] else { return }
+        self.currentlyEnabledLogs = Set(logs.compactMap {
+            EncryptionSessionIdentifier(string: $0)
+        })
+        guard let context = userSession.selfUserClient?.keysStore.encryptionContext
+            else {
+                return
+        }
+        self.currentlyEnabledLogs.forEach {
+            context.setExtendedLogging(identifier: $0, enabled: true)
+        }
     }
-    
-    let user = arguments[0]
-    let client = arguments[1]
-    let identifier = EncryptionSessionIdentifier(
-        userId: user, clientId: client
-    )
-    context.setExtendedLogging(identifier: identifier, enabled: enabled)
-    completionHandler(.Success(info: "Logging " +
-        (enabled ? "enabled" : "disabled") +
-        " for session \(identifier.rawValue)")
-    )
 }
 
 /// Show the user and client identifier
+
 private func showIdentifier(
     arguments: [String],
     userSession: ZMUserSession,
+    commandDefinition: DebugCommandMixin,
     completionHandler: (DebugCommandResult) -> ()
     ) {
     
@@ -184,11 +274,10 @@ private func showIdentifier(
 private func listCommands(
     arguments: [String],
     userSession: ZMUserSession,
+    commandDefinition: DebugCommandMixin,
     completionHandler: (DebugCommandResult) -> ()
     ) {
     
-    let output = ZMUserSession.debugCommands.keys.sorted().map {
-        "\($0) -> \(ZMUserSession.debugCommands[$0]!.description)"
-    }.joined(separator: "\n")
+    let output = userSession.debugCommands.keys.sorted().joined(separator: "\n")
     completionHandler(.Success(info: output))
 }
