80787dddacd3e0d122e65ce0d22ba6128a749be9
John Nguyen <polyxo@protonmail.com>
2021-01-25 10:26:13 +0100

    Add tests

--- Tests/Source/UserSession/ZMUserSessionTests+EncryptionAtRest.swift
+++ Tests/Source/UserSession/ZMUserSessionTests+EncryptionAtRest.swift
@@ -224,5 +224,31 @@ class ZMUserSessionTests_EncryptionAtRest: ZMUserSessionTestsBase {
         XCTAssertFalse(sut.isDatabaseLocked)
         XCTAssertNotEqual(oldKeys, newKeys)
     }
-    
+
+    func testThatIfDatabaseIsLocked_ThenUserSessionLockIsSet() throws {
+        // given
+        simulateLoggedInUser()
+        syncMOC.saveOrRollback()
+        setEncryptionAtRest(enabled: true)
+
+        sut.applicationDidEnterBackground(nil)
+        XCTAssertTrue(waitForAllGroupsToBeEmpty(withTimeout: 0.5))
+
+        XCTAssertTrue(sut.isDatabaseLocked)
+
+        // then
+        XCTAssertEqual(sut.lock, .database)
+    }
+
+    func testThatIfDatabaseIsNotLocked_ThenUserSessionLockIsNotSet() throws {
+        // given
+        simulateLoggedInUser()
+        syncMOC.saveOrRollback()
+        setEncryptionAtRest(enabled: false)
+        XCTAssertFalse(sut.isDatabaseLocked)
+
+        // then
+        XCTAssertNotEqual(sut.lock, .database)
+    }
+
 }
