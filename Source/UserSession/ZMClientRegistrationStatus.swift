a1653bb650fccae5753a98ae1e194530ef5e173b
Bill Chan <billypchan@gmail.com>
2021-06-08 10:18:46 +0200

    converted testThatItReturns_FetchingClients_WhenReceivingAnErrorWithTooManyClients

--- Source/UserSession/ZMClientRegistrationStatus.swift
+++ Source/UserSession/ZMClientRegistrationStatus.swift
@@ -1,9 +1,19 @@
 //
-//  ZMClientRegistrationStatus.swift
-//  WireSyncEngine-ios
+// Wire
+// Copyright (C) 2021 Wire Swiss GmbH
 //
-//  Created by Bill, Yiu Por Chan on 03.06.21.
-//  Copyright © 2021 Zeta Project Gmbh. All rights reserved.
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
 //
 
 import Foundation
@@ -11,7 +21,8 @@ import Foundation
 private let zmLog = ZMSLog(tag: "ZMClientRegistrationStatus")
 
 extension ZMClientRegistrationStatus {
-    func didFail(toRegisterClient error: NSError?) {
+    @objc(didFailToRegisterClient:)
+    public func didFail(toRegisterClient error: NSError?) {
         var error = error
         //we should not reset login state for client registration errors
         zmLog.debug(#function)
